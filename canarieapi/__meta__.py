ee6d39cc88aa405223a20cb06838c4e4839208fb
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-03-18 14:48:10 -0400

    Bump version: 0.3.5 → 0.4.0

--- canarieapi/__meta__.py
+++ canarieapi/__meta__.py
@@ -2,7 +2,7 @@
 General meta information on the canarieapi package.
 """
 
-__version__ = '0.3.5'
+__version__ = '__version__ = "0.4.0"'
 __author__ = "David Byrns"
 __email__ = 'david.byrns@crim.ca'
 __title__ = 'Canarie API'
