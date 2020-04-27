a5acecb4e357629ee4f37425d0e7ad564291943b
pelletfr <francis.pelletier@crim.ca>
2020-04-27 09:39:12 -0400

    changes import and use of 'default_configuration'
    Following discussions, it was deemed a better approach
    to first import and initialize the configuration from the
    'default_configuration' file, which can then be ovewritted
    by with the use of'CANARIE_API_CONFIG_FN' environment
    variable

--- canarieapi/app_object.py
+++ canarieapi/app_object.py
@@ -10,6 +10,7 @@ from flask import Flask
 from os import environ
 
 # -- Project specific --------------------------------------------------------
+from canarieapi import default_configuration
 from canarieapi.reverse_proxied import ReverseProxied
 
 
@@ -24,10 +25,9 @@ APP.logger.setLevel(logging.DEBUG)
 APP.wsgi_app = ReverseProxied(APP.wsgi_app)
 
 # Config
+APP.config.from_object(default_configuration)
 if 'CANARIE_API_CONFIG_FN' in environ:
     APP.config.from_envvar('CANARIE_API_CONFIG_FN')
     APP.logger.info('Loading custom configuration from "{0}"'.format(environ['CANARIE_API_CONFIG_FN']))
 else:
-    from canarieapi import default_configuration
-    APP.config.from_object(default_configuration)
     APP.logger.info('Using default configuration')
