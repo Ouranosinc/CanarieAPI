0cd899b54107882345f3e0f790f652dd7696f0f4
mishaschwartz <4380924+mishaschwartz@users.noreply.github.com>
2023-08-29 14:00:57 -0400

    Bump version: 0.7.2 → 1.0.0

--- canarieapi/__meta__.py
+++ canarieapi/__meta__.py
@@ -2,7 +2,7 @@
 General meta information on the canarieapi package.
 """
 
-__version__ = "0.7.2"
+__version__ = "1.0.0"
 __author__ = "David Byrns, Francis Charette-Migneault"
 __email__ = "francis.charette-migneault@crim.ca"
 __title__ = "Canarie API"
